7ef1525c8efa7de56ef3e776095689936482545e
huydo862003 <huy.do862003@hcmut.edu.vn>
2022-08-30 18:52:40 +0700

    Add Chain Table

---
+++ Data structures/Hash table/Chaining/ChainTable.h
@@ -0,0 +1,62 @@
+#ifndef __CHAIN_TABLE_HELL__
+#define __CHAIN_TABLE_HELL__
+
+#include "../../Linked list/Non-circular doubly linked list/LinkedList.h"
+#include <type_traits>
+#include <stdexcept>
+#include <functional>
+
+template <typename Key_T, typename Value_T, typename Hasher_T = std::hash<Key_T>, std::size_t TABLE_SIZE = 31,
+          typename = std::enable_if_t<std::is_trivially_copyable<Value_T>::value, void>>
+class ChainTable {
+    typedef std::size_t size_t;
+    struct KeyValuePair {
+        Key_T key;
+        Value_T value;
+        bool operator==(const KeyValuePair& other) const {
+            return key == other.key;
+        }
+        bool operator==(const Key_T& key_other) const {
+            return key == key_other;
+        }
+    };
+public:
+    Value_T operator[](const Key_T& key) {
+        const size_t id = hash(key);
+        LinkedList<KeyValuePair>& list = _table[id];        // The slot that `key` hashes to
+
+        auto key_pos = list.search(key);
+        if (!key_pos) throw std::runtime_error{"Indexing on non-existent key."};
+
+        return key_pos->value;
+    }
+
+    void insert(const Key_T& key, const Value_T& value) {
+        const size_t id = hash(key);
+        LinkedList<KeyValuePair>& list = _table[id];        // The slot that `key` hashes to
+        
+        auto key_pos = list.search(key);
+        if (key_pos) throw std::runtime_error{"ChainTable::insert() called on already existed key."};
+        
+        list.push_back({key, value});
+    }
+
+    Value_T remove(const Key_T& key) {
+        const size_t id = hash(key);
+        LinkedList<KeyValuePair>& list = _table[id];        // The slot that `key` hashes to
+
+        auto key_pos = list.search(key);
+        if (!key_pos) throw std::runtime_error{"ChainTable::remove() called on non-existent key."};
+
+        return list.remove(key_pos).value;
+    }
+private:
+    std::size_t hash(const Key_T& key) {
+        return _key_hasher(key) % TABLE_SIZE;
+    }
+
+    LinkedList<KeyValuePair> _table[TABLE_SIZE];
+    const Hasher_T _key_hasher{};
+};
+
+#endif
--- Data structures/README.md
+++ Data structures/README.md
@@ -4,3 +4,6 @@ Data Structures are wonderful and exciting! They're essentially ways of organizi
 
 My goal is to:
 * Implement data structures as generic as possible. But at the same time just as safe - that's why you can see I mostly support [TriviallyCopyableType](https://cplusplus.com/reference/type_traits/is_trivially_copyable/) only (which is a C++ term). Essentially I can treat data as plain data itself, mindlessly moving, copying, swapping them in memory efficiently without too much fuss: with NonTriviallyCopyableType, I have to consider the side effects of default constructors, copy constructors, copy assignments and destructors, whether the copy constructor is deleted, etc.
+
+Things I want to improve:
+* Provide constant version of various class methods where possible. For example: `LinkedList::search`, `ChainTable::search`, .etc.
