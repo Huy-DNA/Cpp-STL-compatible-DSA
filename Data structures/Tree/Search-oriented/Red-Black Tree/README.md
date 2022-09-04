450297ee8cd04b6a64ebbf658c259aff5a6544c5
huydo862003 <huy.do862003@hcmut.edu.vn>
2022-09-04 15:54:45 +0700

    Add RedBlackTree and its README.md

---
+++ Data structures/Tree/Search-oriented/Red-Black Tree/README.md
@@ -0,0 +1,40 @@
+# Red Black Tree
+* Supported operations and their computational complexity: With $n$ being the number of elements,
+    * **SEARCH**: `RedBlackTree::search(element)`
+        * Worst-case auxiliary space: $\Theta(1)$
+        * Worst-case running time: $\Theta(\lg n)$
+    * **INSERT**: `RedBlackTree::insert(element)` 
+        * Worst-case auxiliary space: $\Theta(1)$
+        * Worst-case running time: $\Theta(\lg n)$
+    * **DELETE**: `LinkedList::remove(iterator)`
+        * Worst-case auxiliary space: $\Theta(1)$
+        * Worst-case running time: $\Theta(\lg n)$
+* Other technical:
+    * Only works with elements of `TriviallyCopyableType`.
+
+Things I want to improve:
+* Using `const_cast` is bad and my current design makes it unable to provide const version of all methods of the class!
+* Currently, most class methods accept an argument of a user-defined Iterator type. This type is just acting as a safe wrapper around an internal `Node` pointer so that outside users can not modify the internal data, otherwise, Red Black Tree's assumptions would be violated. However, this leads to the mentioned-above problem and some cumbersome situation, like this one in `RedBlackTree::remove()`:
+```C++
+Node* m = _extract_Node(minimum_at_subtree(_create_ConstBiIter(node->right)));
+```
+
+Things I've learnt anyways:
+* Out-of-template-class definitions of static data members like this do not work:
+```C++
+template <typename Elem_T, typename Comp_T>
+typename RedBlackTree<Elem_T, Comp_T>::Node RedBlackTree<Elem_T, Comp_T>::_sentinel = {{}, _NIL, _NIL, _NIL, BLACK};
+```
+or this:
+```C++
+template <typename Elem_T, typename Comp_T>
+typename RedBlackTree<Elem_T, Comp_T, void>::Node RedBlackTree<Elem_T, Comp_T, void>::_sentinel = {{}, _NIL, _NIL, _NIL, BLACK};
+```
+but this works:
+```C++
+template <typename Elem_T, typename Comp_T, typename _SFINAE>
+typename RedBlackTree<Elem_T, Comp_T, _SFINAE>::Node RedBlackTree<Elem_T, Comp_T, _SFINAE>::_sentinel = {{}, _NIL, _NIL, _NIL, BLACK};
+```
+The only reason I can come up is because these kinds of definition need explicity in the class of which the static data member is being defined. Seems reasonable but not totally convincing.
+
+## Details about Red Black Tree
---
+++ Data structures/Tree/Search-oriented/Red-Black Tree/RedBlackTree.h
@@ -0,0 +1,415 @@
+#ifndef __RED_BLACK_TREE_HELL__
+#define __RED_BLACK_TREE_HELL__
+
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <type_traits>
+#include <string>
+#include <windows.h>            // For colored output
+
+template <typename Elem_T, typename Comp_T = std::less<const Elem_T&>,
+          typename = std::enable_if_t<std::is_trivially_copyable<Elem_T>::value, void>>
+class ConstBiIter;
+
+/*Requires Elem_T to be a trivially copyable type.*/
+template <typename Elem_T, typename Comp_T = std::less<const Elem_T&>,
+          typename = std::enable_if_t<std::is_trivially_copyable<Elem_T>::value, void>>
+class RedBlackTree {
+    friend class ConstBiIter<Elem_T, Comp_T>;
+    typedef std::size_t size_t;
+    typedef ConstBiIter<Elem_T, Comp_T> ConstBiIter;
+
+    enum Color {
+        RED, BLACK, RESET
+    };
+
+    struct Node {
+        Elem_T data;
+        Node* left;
+        Node* right;
+        Node* p;
+        Color color;
+    };
+public:
+    ~RedBlackTree() {
+        _deallocate_node(_root);
+    }
+
+    ConstBiIter search(const Elem_T& e) {
+        Node* cur = _root;
+
+        while (cur != _NIL && cur->data != e)
+            if (_less(cur->data, e)) cur = cur->right;
+            else cur = cur->left;
+
+        return _create_ConstBiIter(cur);               
+    }  
+
+    void insert(const Elem_T& e) {
+        if (_root == _NIL) {
+            _root = new Node{e, _NIL, _NIL, _NIL, BLACK};
+            return;
+        }
+
+        Node* prev;
+        Node* cur = _root;
+
+        while (cur != _NIL) {
+            prev = cur;
+            if (_less(cur->data, e))
+                cur = cur->right;
+            else cur = cur->left;
+        }
+        
+        Node* new_node = new Node{e, _NIL, _NIL, prev, RED};
+        if (_less(prev->data, e))
+            prev->right = new_node;
+        else prev->left = new_node;
+
+        _insert_fixup(new_node);
+    }
+
+    Elem_T remove(const Elem_T& e) {
+        return remove(search(e));
+    }
+
+    Elem_T remove(ConstBiIter iter) {
+        Node* node = _extract_Node(iter);
+
+        if (node == _NIL)
+            throw std::runtime_error{"RedBlackTree::remove() called on null pointer."};
+        
+        Node* to_be_fixed;                  // Node that needs fixing after deletion 
+        Color original_color = node->color; // Color of to be moved-or-removed node
+
+        if (node->left == _NIL) {
+            to_be_fixed = node->right;
+            _transplant(node, node->right);
+        } else if (node->right == _NIL) {
+            to_be_fixed = node->left;
+            _transplant(node, node->left);
+        } else {
+            Node* m = _extract_Node(minimum_at_subtree(_create_ConstBiIter(node->right)));
+            original_color = m->color;
+            to_be_fixed = m->right;
+
+            if (node->right != m) {
+                _transplant(m, m->right);
+                m->right = node->right;
+                m->right->p = m;
+            }
+            _transplant(node, m);
+            m->left = node->left;
+            m->left->p = m;
+            m->color = node->color;
+        }
+        if (original_color == BLACK)
+            _remove_fixup(to_be_fixed);
+        
+        Elem_T res = node->data;
+        delete node;
+        return res;
+    }
+
+    ConstBiIter minimum_at_subtree(ConstBiIter iter) {
+        const Node* cur = _extract_Node(iter);
+        if (cur == _NIL) return _create_ConstBiIter(_NIL);
+
+        while (cur->left != _NIL)
+            cur = cur->left;
+        return _create_ConstBiIter(cur);
+    }
+
+    ConstBiIter maximum_at_subtree(ConstBiIter iter) {
+        const Node* cur = _extract_Node(iter);
+        if (cur == _NIL) return _create_ConstBiIter(_NIL);
+
+        while (cur->right != _NIL)
+            cur = cur->right;
+        return _create_ConstBiIter(cur);
+    }
+
+    ConstBiIter maximum() {
+        return maximum_at_subtree(_root);
+    }
+
+    ConstBiIter minimum() {
+        return minimum_at_subtree(_root);
+    }
+    
+    ConstBiIter successor(ConstBiIter iter) {
+        const Node* node = _extract_Node(iter);
+        if (node == _NIL)
+            return _create_ConstBiIter(_NIL);
+        if (node->right != _NIL)
+            return minimum_at_subtree(node->right);
+        while (node != _NIL && node->p->left != node)
+            node = node->p;
+        if (node == _NIL)
+            return _create_ConstBiIter(_NIL);
+        return _create_ConstBiIter(node->p);
+    }
+
+    ConstBiIter predecessor(ConstBiIter iter) {
+        const Node* node = _extract_Node(iter);
+        if (node == _NIL)
+            return _create_ConstBiIter(_NIL);
+        if (node->left != _NIL)
+            return maximum_at_subtree(node->right);
+        while (node != _NIL && node->p->right != node)
+            node = node->p;
+        if (node == _NIL)
+            return _create_ConstBiIter(_NIL);
+        return _create_ConstBiIter(node->p);
+    }
+
+    void print() const {
+        _print_recur("", _root, false);
+    }
+private:
+    void _print_recur(const std::string& prefix, const Node* node, bool isLeft) const {
+        /*From: https://stackoverflow.com/questions/36802354/print-binary-tree-in-a-pretty-way-using-c*/
+        if (node != _NIL) {
+            std::cout << prefix;
+
+            std::cout << (isLeft ? "├──" : "└──" );
+
+            // print the value of the node
+            _set_text_color(node->color);
+            std::cout << node->data << '\n';
+            _set_text_color(RESET);
+
+            // enter the next tree level - left and right branch
+            _print_recur(prefix + (isLeft ? "│   " : "    "), node->left, true);
+            _print_recur(prefix + (isLeft ? "│   " : "    "), node->right, false);
+        }
+    }
+
+    static void _set_text_color(Color color) {
+        static HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+        int code;
+        switch (color) {
+            case BLACK: code = 0; break;
+            case RED: code = 4; break;
+            default: code = 7; break;
+        }
+        SetConsoleTextAttribute(h, code);
+    }
+
+    void _deallocate_node(Node* n) {
+        if (n == _NIL) return;
+        _deallocate_node(n->left);
+        _deallocate_node(n->right);
+        delete n;
+    }
+
+    void _insert_fixup(Node* n) {
+        while (n->p->color == RED) {
+            Node* m = n->p;         // n's parent
+                                    // m is RED
+                                    // m->p is BLACK
+            if (m->p->left == m) {
+                Node* u = m->p->right;
+                if (u->color == RED) {
+                    m->color = u->color = BLACK;
+                    m->p->color = RED;
+                    n = m->p;
+                } else {
+                    if (m->right == n) {
+                        _left_rotate(m);
+                        n = m;
+                        m = n->p;
+                    }
+                    m->color = BLACK;
+                    m->p->color = RED;
+                    _right_rotate(m->p);
+                    return;
+                }
+            } else {
+                Node* u = m->p->left;
+                if (u->color == RED) {
+                    m->color = u->color = BLACK;
+                    m->p->color = RED;
+                    n = m->p;
+                } else {
+                    if (m->left == n) {
+                        _right_rotate(m);
+                        n = m;
+                        m = n->p;
+                    }
+                    m->color = BLACK;
+                    m->p->color = RED;
+                    _left_rotate(m->p);
+                    return;
+                }
+            }
+        }
+        _root->color = BLACK;
+    }
+
+    void _remove_fixup(Node* n) {
+        while (n->color == BLACK && n != _root) {
+            Node* m = n->p;         // n's parent
+            if (m->left == n) {
+                Node* u = m->right;
+                if (u->color == RED) {
+                    m->color = RED;
+                    u->color = BLACK;
+                    _left_rotate(m);
+                    u = m->right;
+                }
+                if (u->left->color == BLACK && u->right->color == BLACK) {
+                    u->color = RED;
+                    n = m;
+                } else {
+                    if (u->right->color == BLACK) {
+                        u->left->color = BLACK;
+                        u->color = RED;
+                        _right_rotate(u);
+                        u = u->p;
+                    }
+                    u->right->color = BLACK;
+                    u->color = m->color;
+                    m->color = BLACK;
+                    _left_rotate(m);
+                    n = _root;
+                }
+            } else {
+                Node* u = m->left;
+                if (u->color == RED) {
+                    m->color = RED;
+                    u->color = BLACK;
+                    _right_rotate(m);
+                    u = m->left;
+                }
+                if (u->left->color == BLACK && u->right->color == BLACK) {
+                    u->color = RED;
+                    n = m;
+                } else {
+                    if (u->left->color == BLACK) {
+                        u->right->color = BLACK;
+                        u->color = RED;
+                        _left_rotate(u);
+                        u = u->p;
+                    }
+                    u->left->color = BLACK;
+                    u->color = m->color;
+                    m->color = BLACK;
+                    _right_rotate(m);
+                    n = _root;
+                }
+            }
+        }
+        n->color = BLACK;   
+    }
+
+    void _transplant(Node* u, Node* v) {
+        v->p = u->p;
+
+        if (u->p == _NIL)
+            _root = v;
+        else if (u->p->left == u)
+            u->p->left = v;
+        else u->p->right = v;
+    }
+
+    void _left_rotate(Node* u) {
+        if (u->right == _NIL) return;
+        Node* v = u->right;
+        
+        v->p = u->p;
+        
+        if (u->p == _NIL)
+            _root = v;
+        else if (u->p->left == u)
+            u->p->left = v;
+        else u->p->right = v;
+
+        u->right = v->left;
+        v->left = u;
+        u->right->p = u;
+        u->p = v;
+    }
+
+    void _right_rotate(Node* u) {
+        if (u->left == _NIL) return;
+        Node* v = u->left;
+        
+        v->p = u->p;
+        
+        if (u->p == _NIL)
+            _root = v;
+        else if (u->p->left == u)
+            u->p->left = v;
+        else u->p->right = v;
+
+        u->left = v->right;
+        v->right = u;
+        u->left->p = u;
+        u->p = v;
+    }
+
+    ConstBiIter _create_ConstBiIter(const Node* node) {
+        return {node, *this};
+    }
+
+    Node* _extract_Node(ConstBiIter iter) {
+        return const_cast<Node*>(iter._node);       //Bad: ConstBiIter cast but...
+    }
+    Node* _root = _NIL;
+    const Comp_T _less{};
+    static Node _sentinel;
+    static constexpr Node* _NIL = &_sentinel;
+};
+
+template <typename Elem_T, typename Comp_T, typename _SFINAE>
+typename RedBlackTree<Elem_T, Comp_T, _SFINAE>::Node RedBlackTree<Elem_T, Comp_T, _SFINAE>::_sentinel = {{}, _NIL, _NIL, _NIL, BLACK};
+
+template <typename Elem_T, typename Comp_T,
+          typename>
+class ConstBiIter {
+    friend class RedBlackTree<Elem_T, Comp_T>;
+    typedef typename RedBlackTree<Elem_T, Comp_T>::Node Node;
+    static constexpr Node* _NIL = RedBlackTree<Elem_T, Comp_T>::_NIL;
+public:
+    const Elem_T& operator*() const {
+        if (_node == _NIL)
+            throw std::runtime_error{"Dereferencing nullptr."};
+        return _node->data;
+    }
+
+    const Elem_T* operator->() const {
+        if (_node == _NIL)
+            throw std::runtime_error{"Dereferencing nullptr."};
+        return &(_node->data);
+    }
+
+    ConstBiIter operator++() {
+        _node = _owner.successor({_node, _owner})._node;
+        return *this;
+    }
+
+    ConstBiIter operator--() {
+        _node = _owner.predecessor({_node, _owner})._node;
+        return *this;
+    }
+
+    bool operator==(const ConstBiIter& other) const {
+        return _node == other._node;
+    }
+private:
+    ConstBiIter(const Node* node, const RedBlackTree<Elem_T, Comp_T>& owner): _node{node}, _owner{owner} {}
+    const Node* _node;
+    const RedBlackTree<Elem_T, Comp_T>& _owner;
+};
+
+template <typename Elem_T, typename Comp_T>
+struct std::iterator_traits<ConstBiIter<Elem_T, Comp_T>> {
+    typedef const std::remove_cv_t<Elem_T> value_type;                //cppreference does not specify what this does so...
+    typedef const Elem_T* pointer;                                    //cppreference does not specify what this does so...
+    typedef const Elem_T& reference;                                  //cppreference does not specify what this does so...
+    typedef std::bidirectional_iterator_tag iterator_category;        //I know what this does!!!
+};
+
+#endif
